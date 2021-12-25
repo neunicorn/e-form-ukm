0c46bfa177f10b5e9965d0c95986af6541e2077b
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2021-12-25 17:43:27 +0700

    membenarkan database php

--- backend/regis.php
+++ backend/regis.php
@@ -1,25 +1,27 @@
 <?php
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $ukm = $_POST['ukm'];
-    $nim = $_POST['nim'];
-    $email = $_POST['email'];
-    $fakultas = $_POST['fakultas'];
-    $prodi = $_POST['prodi'];
-    $nohp = $_POST['nohp'];
-    $alasan = $_POST['alasan'];
 
-    //database connections
-    $conn = new mysqli('localhost:3308', 'root', '', 'formukm');
-    if($conn->connect_error){
-        die('Connection Failed : '.$conn->connect_error );
-    }else{
-        $stmt = $conn->prepare("insert into dataregis(firstName, lastName, ukm, nim, email, fakultas, prodi, nohp, alasan)
-        values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssssssss", $firstName, $lastName, $ukm, $nim, $email, $fakultas, $prodi, $nohp, $alasan);
-        $stmt->execute();
-        echo "registration success";
-        $stmt->close();
-        $conn->close();
-    }
+$conn = mysqli_connect('localhost:3308', 'root', '', 'formukm');
+if(!$conn){
+    die('gagal connect' .$conn->connect_error());
+}
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$ukm = $_POST['ukm'];
+$nim = $_POST['nim'];
+$email = $_POST['email'];
+$fakultas = $_POST['fakultas'];
+$prodi = $_POST['prodi'];
+$nohp = $_POST['nohp'];
+$alasan = $_POST['alasan'];
+
+$querry = mysqli_query($conn, "INSERT INTO dataregis(firstName, lastName, ukm, nim, email, fakultas, prodi, nohp, alasan)
+    VALUES ('$firstName', '$lastName', '$ukm', '$nim', '$email', '$fakultas', '$prodi', '$nohp', '$alasan')");
+
+if($querry){
+    header("location: ../index.html?message=berhasil");
+}else{
+    header("location: ../index.html?message=gagal");
+}
+
 ?>
